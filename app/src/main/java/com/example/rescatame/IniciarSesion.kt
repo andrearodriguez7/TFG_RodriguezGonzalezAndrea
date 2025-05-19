8ba51018efb7ce912e0afd9a97d80559077e8e54
Kike Bellido <ebellidoperejil2@gmail.com>
2025-05-19 20:44:21 +0200

    Subida de la app

---
+++ app/src/main/java/com/example/rescatame/IniciarSesion.kt
@@ -0,0 +1,32 @@
+package com.example.rescatame
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class IniciarSesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.iniciarsesion_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvCrearCuenta = findViewById<TextView>(R.id.tvCrearCuenta)
+
+        tvCrearCuenta.setOnClickListener{
+
+            val intent = Intent(this, Registro::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
